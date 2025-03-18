f7cfd15d7eadcedcb7458f820e94f212f0f4a947
Armaan <you@example.com>
2025-03-18 11:59:38 +0530

    added functionality related sign in and sign up

--- lib/presentation/widgets/textfield.dart
+++ lib/presentation/widgets/textfield.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:oculoo02/core/configs/theme/app_color.dart';
 
-class Textfield extends StatelessWidget {
+class Textfield extends StatefulWidget {
   final String lbl;
   final IconData? icon;
   final bool obscureText;
@@ -15,17 +15,40 @@ class Textfield extends StatelessWidget {
       this.obscureText = false});
 
   @override
+  State<Textfield> createState() => _TextfieldState();
+}
+
+class _TextfieldState extends State<Textfield> {
+  late bool _obscureText;
+
+  @override
+  void initState() {
+    super.initState();
+    _obscureText = widget.obscureText;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
       child: TextField(
-        controller: controller,
-        obscureText: obscureText,
+        controller: widget.controller,
+        obscureText: _obscureText,
         keyboardType: TextInputType.text,
         autofocus: false,
         decoration: InputDecoration(
-          suffixIcon: Icon(icon),
-          hintText: lbl,
+          suffixIcon: widget.obscureText
+              ? IconButton(
+                  icon: Icon(
+                      _obscureText ? Icons.visibility_off : Icons.visibility),
+                  onPressed: () {
+                    setState(() {
+                      _obscureText = !_obscureText;
+                    });
+                  },
+                )
+              : (widget.icon != null ? Icon(widget.icon) : null),
+          hintText: widget.lbl,
           hintStyle: TextStyle(color: AppColor.darkgrey),
           fillColor: AppColor.grey,
           filled: true,
